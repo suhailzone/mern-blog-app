1d2cc181572f549fbd7f2653244bf83cb962984e
Sohail Shaikh <sohailshaikh07899@gmail.com>
2020-03-24 03:21:52 +0530

    css changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+
 class App extends Component {
 
   constructor(props) {
@@ -78,27 +79,29 @@ class App extends Component {
   // }
 
   render() {
-    console.log(this.state)
     return (
-      <div className="body">
-        <h2>Welcome to my App</h2>
-        <form onSubmit={this.formSubmission}>
+      <div>
+        <h2>Welcome to BlogRoom</h2>
+        <h3 className="blog-add">Add Blog Post:</h3>
+        <form onSubmit={this.formSubmission} className="form">
           <div className="form-input">
             <input type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.handleChanges} />
           </div>
           <div className="form-input">
             <textarea value={this.state.body} placeholder="Body" onChange={this.handleChanges} name="body"></textarea>
           </div>    
-          <button type="submit">Submit</button>
+          <button type="submit">Post</button>
         </form>
 
-        <div>
+        <h3 className="blog-list-title">Post List:</h3>
+        <div className="blog-list">          
           {
             this.state.posts.map((post) => {
               return (
                 <div key={post._id} className="posts">
                   <h3>{post.title}</h3>
                   <p>{post.body}</p>
+                  <hr />
                 </div>
               )
             })
